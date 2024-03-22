1ad9677f4d341d8633fdba40f7cf1924a78b63ef
= <lincon_martins@sicredi.com.br>
2024-03-22 15:52:10 -0300

    feature: Criação do projeto

---
+++ engineering/src/main/java/br/com/engineering/EngineeringApplication.java
@@ -0,0 +1,13 @@
+package br.com.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringApplication.class, args);
+	}
+
+}
